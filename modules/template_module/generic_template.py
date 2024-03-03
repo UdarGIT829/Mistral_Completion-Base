267771b47358b7d6e349cf27244e583ae0462087
UdarGIT829 <viraatudar9@gmail.com>
2024-03-03 14:52:37 -0800

    Make generic template for Base

---
+++ modules/template_module/generic_template.py
@@ -0,0 +1,12 @@
+from modules.instr_cls import instruct_template_cls
+
+def get_template()->instruct_template_cls:
+    return instruct_template_cls(  name="instruct template", 
+                        template="""[INST] You are a helpful AI assistant. You will think outside of the box to help the user with their request:
+#CHUNK
+[/INST]
+Assistant: "
+""", 
+                        replaceAmt=1, 
+                        replacePrompt=["#CHUNK"]
+                    )
